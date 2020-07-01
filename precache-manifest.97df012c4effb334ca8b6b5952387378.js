a7da8a4285d4b7511846b6c1e3008f83adfe861c
Ankit Kumar Sahoo <10ankitkusahoo10@gmail.com>
2020-07-01 17:56:09 +0530

    Updates

---
+++ precache-manifest.97df012c4effb334ca8b6b5952387378.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71450aa6cdc25565acd3d496e7130b9d",
+    "url": "/ankit-ku-sahoo/react-app/index.html"
+  },
+  {
+    "revision": "816140af8c02bc49bcc8",
+    "url": "/ankit-ku-sahoo/react-app/static/css/main.8cdd916a.chunk.css"
+  },
+  {
+    "revision": "474bc7f19ac255ebd7cc",
+    "url": "/ankit-ku-sahoo/react-app/static/js/2.ea8b9b8f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ankit-ku-sahoo/react-app/static/js/2.ea8b9b8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "816140af8c02bc49bcc8",
+    "url": "/ankit-ku-sahoo/react-app/static/js/main.ce27d76d.chunk.js"
+  },
+  {
+    "revision": "195142588e57c6456db4",
+    "url": "/ankit-ku-sahoo/react-app/static/js/runtime-main.b67db78f.js"
+  },
+  {
+    "revision": "4e4f318db65c91ea4465b3b6862a6be0",
+    "url": "/ankit-ku-sahoo/react-app/static/media/Chris.4e4f318d.jpg"
+  },
+  {
+    "revision": "d1259bc6a177289031cb8bc941721245",
+    "url": "/ankit-ku-sahoo/react-app/static/media/Competitive-Programming-1.d1259bc6.jpg"
+  },
+  {
+    "revision": "d90a2d9c680b7843b8eb20ae8add0917",
+    "url": "/ankit-ku-sahoo/react-app/static/media/background.d90a2d9c.jpg"
+  },
+  {
+    "revision": "7c4e2f322fc1a6181186f82d824b5d4f",
+    "url": "/ankit-ku-sahoo/react-app/static/media/blue-body-of-water-with-orange-thunder-1102915.7c4e2f32.jpg"
+  },
+  {
+    "revision": "f57fd5dc368beacc17212192cd36c85a",
+    "url": "/ankit-ku-sahoo/react-app/static/media/data-structure.f57fd5dc.jpg"
+  },
+  {
+    "revision": "b21b0cdf7b3e619992ed192867b0fe8e",
+    "url": "/ankit-ku-sahoo/react-app/static/media/data-structures-training-in-gurgaon-500x500.b21b0cdf.png"
+  },
+  {
+    "revision": "6e1267d9d946b0236cdf6ffd02890894",
+    "url": "/ankit-ku-sahoo/react-app/static/media/favicon.6e1267d9.ico"
+  },
+  {
+    "revision": "e4c3d91a4847a162fdd3689492cef389",
+    "url": "/ankit-ku-sahoo/react-app/static/media/happy-adult-woman-reading-book-with-interest-on-street-3772711.e4c3d91a.jpg"
+  },
+  {
+    "revision": "2efd6384dfdfe71fd1b307ab3402648a",
+    "url": "/ankit-ku-sahoo/react-app/static/media/man-beside-flat-screen-television-with-photos-background-716276.2efd6384.jpg"
+  },
+  {
+    "revision": "2a12928e7425007b64bcd49a7f01cdcf",
+    "url": "/ankit-ku-sahoo/react-app/static/media/man-in-beige-blazer-holding-tablet-computer-3184328.2a12928e.jpg"
+  },
+  {
+    "revision": "5a6848218b74011ef4c29a1eac2a979d",
+    "url": "/ankit-ku-sahoo/react-app/static/media/man-in-black-polo-standing-in-front-of-a-class-3981797.5a684821.jpg"
+  },
+  {
+    "revision": "54936ab76a91e6478e67fba4c7c7e6c0",
+    "url": "/ankit-ku-sahoo/react-app/static/media/maxresdefault.54936ab7.jpg"
+  },
+  {
+    "revision": "d25eda7cee0730b82b2c5862e7338086",
+    "url": "/ankit-ku-sahoo/react-app/static/media/ntnew-16_31_342479060applied-maths.d25eda7c.jpg"
+  },
+  {
+    "revision": "f1a9a80414cff529cac405e2ae12c785",
+    "url": "/ankit-ku-sahoo/react-app/static/media/photo-of-woman-teaching-935943.f1a9a804.jpg"
+  },
+  {
+    "revision": "8e4501e0c206f94fad9490701be21383",
+    "url": "/ankit-ku-sahoo/react-app/static/media/silhouttes-of-mountains-3209168.8e4501e0.jpg"
+  }
+]);
